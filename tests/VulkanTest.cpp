d21397748ccf9b152b8d7751da731f7b37a16f6f
joechu <chuzcjoe@gmail.com>
2025-08-24 00:09:17 -0700

    Update VulkanTest.cpp

--- tests/VulkanTest.cpp
+++ tests/VulkanTest.cpp
@@ -9,11 +9,11 @@ namespace test {
 
 TEST(VulkanTest, test) {
   core::vulkan::VulkanContext context(true, core::vulkan::QueueFamilyType::Compute);
-  EXPECT_TRUE(context.instance != VK_NULL_HANDLE);
-
   core::vulkan::VulkanBuffer buffer(
       &context, 3, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
       VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+  EXPECT_TRUE(context.instance != VK_NULL_HANDLE);
 }
 }  // namespace test
 }  // namespace core
