8824e99669207668816e9b24436e17ba58f1886e
joechu <chuzcjoe@gmail.com>
2025-08-24 22:01:51 -0700

    Update MatTest.cpp

--- tests/MatTest.cpp
+++ tests/MatTest.cpp
@@ -10,10 +10,17 @@ TEST(MatTest, test) {
   mat.Fill(1.0f);
   *mat(0, 1) = 2.0f;
 
+  core::Mat<float, 3> mat_copy = mat;
+
   EXPECT_EQ(mat.rows(), 2);
   EXPECT_EQ(mat.cols(), 2);
   EXPECT_EQ(*mat(0, 0), 1.0f);
   EXPECT_EQ(*mat(0, 1), 2.0f);
+
+  EXPECT_EQ(mat_copy.rows(), 2);
+  EXPECT_EQ(mat_copy.cols(), 2);
+  EXPECT_EQ(*mat_copy(0, 0), 1.0f);
+  EXPECT_EQ(*mat_copy(0, 1), 2.0f);
 }
 
 }  // namespace test
