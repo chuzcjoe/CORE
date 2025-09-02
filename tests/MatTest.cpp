05c2256fe9f9ddc24a660c8dafa09bfa646b41df
chuzcjoe <chuzcjoe@gmail.com>
2025-09-01 23:01:23 -0700

    add clone() for Mat

--- tests/MatTest.cpp
+++ tests/MatTest.cpp
@@ -23,5 +23,19 @@ TEST(MatTest, test) {
   EXPECT_EQ(*mat_copy(0, 1), 2.0f);
 }
 
+TEST(MatTest2, test) {
+  core::Mat<float, 3> mat(2, 2);
+  mat.Fill(1.0f);
+  *mat(0, 1) = 2.0f;
+
+  core::Mat<float, 3> mat_copy = mat.clone();
+
+  auto* ptr1 = mat.data();
+  auto* ptr2 = mat_copy.data();
+  for (int i = 0; i < mat.total(); ++i) {
+    EXPECT_EQ(ptr1[i], ptr2[i]);
+  }
+}
+
 }  // namespace test
 }  // namespace core
