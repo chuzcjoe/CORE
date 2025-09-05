0428846ed28e0f4fc3f337deef838fdbcddbbf7b
chuzcjoe <chuzcjoe@gmail.com>
2025-09-04 22:46:49 -0700

    add VulkanGraphic

---
+++ vulkan/include/VulkanGraphic.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+
+#include "VulkanBase.h"
+#include "VulkanBuffer.h"
+#include "VulkanContext.h"
+#include "VulkanImage.h"
+#include "glm/glm.hpp"
+
+namespace core {
+namespace vulkan {
+
+// struct Vertex {
+//   glm::vec2 pos;
+//   glm::vec3 color;
+// };
+
+class VulkanGraphic : public VulkanBase {
+ public:
+  VulkanGraphic(VulkanContext* context, VkRenderPass render_pass);
+
+ protected:
+  void CreatePipeline() override;
+
+ private:
+  // Derived class needs to configure these explicitly
+  virtual const std::vector<uint32_t> LoadVertexShader() const = 0;
+  virtual const std::vector<uint32_t> LoadFragmentShader() const = 0;
+  virtual std::vector<VkVertexInputBindingDescription> GetVertexBindingDescriptions() const = 0;
+  virtual std::vector<VkVertexInputAttributeDescription> GetVertexAttributeDescriptions() const = 0;
+
+  VkRenderPass render_pass_;
+};
+
+}  // namespace vulkan
+}  // namespace core
