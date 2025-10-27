3f7232c3dc2a8a48a056671c385d0245063b5e3a
chuzcjoe <chuzcjoe@gmail.com>
2025-10-27 10:18:39 -0700

    add comment

--- examples/opengl/GLDepthStencilDemo/main.cpp
+++ examples/opengl/GLDepthStencilDemo/main.cpp
@@ -154,13 +154,13 @@ int main() {
     
   float plane_vertices[] = {
         // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
-         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
-        -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
-        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+         5.0f, -0.5f - 0.01f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f - 0.01f,  5.0f,  0.0f, 0.0f,
+        -5.0f, -0.5f - 0.01f, -5.0f,  0.0f, 2.0f, // add a small offset to the floor to prevent z-fighting
 
-         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
-        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
-         5.0f, -0.5f, -5.0f,  2.0f, 2.0f								
+         5.0f, -0.5f - 0.01f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f - 0.01f, -5.0f,  0.0f, 2.0f,
+         5.0f, -0.5f - 0.01f, -5.0f,  2.0f, 2.0f								
   };
   // clang-format on
 
