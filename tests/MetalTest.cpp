05e173087abc213c97b6fe1ae182f0fda861e95e
chuzcjoe <chuzcjoe@gmail.com>
2025-11-04 23:25:03 -0800

    add metal support

---
+++ tests/MetalTest.cpp
@@ -0,0 +1,25 @@
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#include <gtest/gtest.h>
+
+#include <iostream>
+
+#include "Foundation/Foundation.hpp"
+#include "Metal/Metal.hpp"
+#include "QuartzCore/QuartzCore.hpp"
+
+namespace core {
+namespace test {
+
+TEST(Metal, test) {
+  MTL::Device* metalDevice = MTL::CreateSystemDefaultDevice();
+  if (metalDevice == nullptr) {
+    std::cout << "Metal is not supported on this system.\n";
+  } else {
+    std::cout << "Metal device name: " << metalDevice->name()->utf8String() << "\n";
+  }
+  EXPECT_EQ(metalDevice != nullptr, true);
+}
+}  // namespace test
+}  // namespace core
