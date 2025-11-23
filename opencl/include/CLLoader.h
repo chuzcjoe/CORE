4c889c49908b4ae7e5f8b6f42d86ca05e7985bb8
Joe <44826941+chuzcjoe@users.noreply.github.com>
2025-11-22 23:58:46 -0800

    Load more opencl functions and add vec add test (#51)
    Co-authored-by: chuzcjoe <chuzcjoe@gmail.com>

--- opencl/include/CLLoader.h
+++ opencl/include/CLLoader.h
@@ -31,7 +31,26 @@ typedef void* CORE_DYNLIB_HANDLE;
 #define CL_ERROR_OPEN_FAILED -1    // fail to open the OpenCL dynamic library
 #define CL_ERROR_ATEXIT_FAILED -2  // fail to register atexit handler
 
-#define clGetPlatformIDs CL_GET_FUN(core::opencl::__clGetPlatformIDs)
+// clang-format off
+#define clGetPlatformIDs          CL_GET_FUN(core::opencl::__clGetPlatformIDs)
+#define clGetDeviceIDs            CL_GET_FUN(core::opencl::__clGetDeviceIDs)
+#define clCreateContext           CL_GET_FUN(core::opencl::__clCreateContext)
+#define clCreateCommandQueue      CL_GET_FUN(core::opencl::__clCreateCommandQueue)
+#define clCreateProgramWithSource CL_GET_FUN(core::opencl::__clCreateProgramWithSource)
+#define clBuildProgram            CL_GET_FUN(core::opencl::__clBuildProgram)
+#define clCreateKernel            CL_GET_FUN(core::opencl::__clCreateKernel)
+#define clCreateBuffer            CL_GET_FUN(core::opencl::__clCreateBuffer)
+#define clSetKernelArg            CL_GET_FUN(core::opencl::__clSetKernelArg)
+#define clEnqueueNDRangeKernel    CL_GET_FUN(core::opencl::__clEnqueueNDRangeKernel)
+#define clFinish                  CL_GET_FUN(core::opencl::__clFinish)
+#define clEnqueueReadBuffer       CL_GET_FUN(core::opencl::__clEnqueueReadBuffer)
+#define clReleaseMemObject        CL_GET_FUN(core::opencl::__clReleaseMemObject)
+#define clReleaseKernel           CL_GET_FUN(core::opencl::__clReleaseKernel)
+#define clReleaseProgram          CL_GET_FUN(core::opencl::__clReleaseProgram)
+#define clReleaseCommandQueue     CL_GET_FUN(core::opencl::__clReleaseCommandQueue)
+#define clReleaseContext          CL_GET_FUN(core::opencl::__clReleaseContext)
+#define clGetProgramBuildInfo     CL_GET_FUN(core::opencl::__clGetProgramBuildInfo)
+// clang-format on
 
 namespace core {
 namespace opencl {
@@ -43,7 +62,95 @@ int cl_init();
 typedef cl_int (*PFN_CLGETPLATFORMIDS)(cl_uint /* num_entries */, cl_platform_id* /* platforms */,
                                        cl_uint* /* num_platforms */) CL_API_SUFFIX__VERSION_1_0;
 
-extern PFN_CLGETPLATFORMIDS __clGetPlatformIDs;
+typedef cl_int (*PFN_CLGETDEVICEIDS)(cl_platform_id /* platform */,
+                                     cl_device_type /* device_type */, cl_uint /* num_entries */,
+                                     cl_device_id* /* devices */,
+                                     cl_uint* /* num_devices */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_context (*PFN_CLCREATECONTEXT)(
+    const cl_context_properties* /* properties */, cl_uint /* num_devices */,
+    const cl_device_id* /* devices */,
+    void (* /* pfn_notify */)(const char*, const void*, size_t, void*), void* /* user_data */,
+    cl_int* /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_command_queue (*PFN_CLCREATECOMMANDQUEUE)(
+    cl_context /* context */, cl_device_id /* device */,
+    cl_command_queue_properties /* properties */,
+    cl_int* /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_program (*PFN_CLCREATEPROGRAMWITHSOURCE)(
+    cl_context /* context */, cl_uint /* count */, const char** /* strings */,
+    const size_t* /* lengths */, cl_int* /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLBUILDPROGRAM)(cl_program /* program */, cl_uint /* num_devices */,
+                                     const cl_device_id* /* device_list */,
+                                     const char* /* options */,
+                                     void(CL_CALLBACK* /* pfn_notify */)(cl_program /* program */,
+                                                                         void* /* user_data */),
+                                     void* /* user_data */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_kernel (*PFN_CLCREATEKERNEL)(cl_program /* program */, const char* /* kernel_name */,
+                                        cl_int* /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_mem (*PFN_CLCREATEBUFFER)(cl_context /* context */, cl_mem_flags /* flags */,
+                                     size_t /* size */, void* /* host_ptr */,
+                                     cl_int* /* errcode_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLSETKERNELARG)(cl_kernel /* kernel */, cl_uint /* arg_index */,
+                                     size_t /* arg_size */,
+                                     const void* /* arg_value */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLENQUEUENDRANGEKERNEL)(
+    cl_command_queue /* command_queue */, cl_kernel /* kernel */, cl_uint /* work_dim */,
+    const size_t* /* global_work_offset */, const size_t* /* global_work_size */,
+    const size_t* /* local_work_size */, cl_uint /* num_events_in_wait_list */,
+    const cl_event* /* event_wait_list */, cl_event* /* event */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLFINISH)(cl_command_queue /* command_queue */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLENQUEUEREADBUFFER)(cl_command_queue /* command_queue */, cl_mem /* buffer */,
+                                          cl_bool /* blocking_read */, size_t /* offset */,
+                                          size_t /* cb */, void* /* ptr */,
+                                          cl_uint /* num_events_in_wait_list */,
+                                          const cl_event* /* event_wait_list */,
+                                          cl_event* /* event */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLGETPROGRAMBUILDINFO)(
+    cl_program /* program */, cl_device_id /* device */, cl_program_build_info /* param_name */,
+    size_t /* param_value_size */, void* /* param_value */,
+    size_t* /* param_value_size_ret */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLRELEASEMEMOBJECT)(cl_mem /* memobj */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLRELEASEKERNEL)(cl_kernel /* kernel */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLRELEASEPROGRAM)(cl_program /* program */) CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLRELEASECOMMANDQUEUE)(cl_command_queue /* command_queue */)
+    CL_API_SUFFIX__VERSION_1_0;
+
+typedef cl_int (*PFN_CLRELEASECONTEXT)(cl_context /* context */) CL_API_SUFFIX__VERSION_1_0;
+
+// clang-format off
+extern PFN_CLGETPLATFORMIDS          __clGetPlatformIDs;
+extern PFN_CLGETDEVICEIDS            __clGetDeviceIDs;
+extern PFN_CLCREATECONTEXT           __clCreateContext;
+extern PFN_CLCREATECOMMANDQUEUE      __clCreateCommandQueue;
+extern PFN_CLCREATEPROGRAMWITHSOURCE __clCreateProgramWithSource;
+extern PFN_CLBUILDPROGRAM            __clBuildProgram;
+extern PFN_CLCREATEKERNEL            __clCreateKernel;
+extern PFN_CLCREATEBUFFER            __clCreateBuffer;
+extern PFN_CLSETKERNELARG            __clSetKernelArg;
+extern PFN_CLENQUEUENDRANGEKERNEL    __clEnqueueNDRangeKernel;
+extern PFN_CLFINISH                  __clFinish;
+extern PFN_CLENQUEUEREADBUFFER       __clEnqueueReadBuffer;
+extern PFN_CLGETPROGRAMBUILDINFO     __clGetProgramBuildInfo;
+extern PFN_CLRELEASEMEMOBJECT        __clReleaseMemObject;
+extern PFN_CLRELEASEKERNEL           __clReleaseKernel;
+extern PFN_CLRELEASEPROGRAM          __clReleaseProgram;
+extern PFN_CLRELEASECOMMANDQUEUE     __clReleaseCommandQueue;
+extern PFN_CLRELEASECONTEXT          __clReleaseContext;
+// clang-format on
 
 }  // namespace opencl
 }  // namespace core
--- tests/CLTest.cpp
+++ tests/CLTest.cpp
@@ -23,5 +23,100 @@ TEST(OpenCL, test1) {
   std::cout << "num platforms: " << num_platforms << std::endl;
 }
 
+TEST(OpenCL, VecAdd) {
+  // 1. Input data
+  const int N = 8;
+  std::vector<float> A(N), B(N), C(N);
+  for (int i = 0; i < N; ++i) {
+    A[i] = static_cast<float>(i);
+    B[i] = static_cast<float>(i * 10);
+    std::cout << "A[" << i << "] = " << A[i] << ", B[" << i << "] = " << B[i] << '\n';
+  }
+
+  // 2. Kernel source embedded as string literal
+  const char* kernelSrc = R"CLC(
+        __kernel void vec_add(__global const float* A,
+                              __global const float* B,
+                              __global float* C) {
+            int i = get_global_id(0);
+            C[i] = A[i] + B[i];
+        }
+    )CLC";
+
+  // initialize OpenCL
+  int init = core::opencl::cl_init();
+  if (init) {
+    throw std::runtime_error("Failed to initialize OpenCL loader");
+  }
+
+  // 3. Discover platform and device
+  cl_int err = CL_SUCCESS;
+  cl_uint numPlatforms = 0;
+  clGetPlatformIDs(0, nullptr, &numPlatforms);
+  std::vector<cl_platform_id> platforms(numPlatforms);
+  clGetPlatformIDs(numPlatforms, platforms.data(), nullptr);
+  cl_platform_id platform = platforms[0];
+
+  cl_uint numDevices = 0;
+  clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, nullptr, &numDevices);
+  std::vector<cl_device_id> devices(numDevices);
+  clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, numDevices, devices.data(), nullptr);
+  cl_device_id device = devices[0];
+
+  // 4. Create context and command queue
+  cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &err);
+  cl_command_queue queue = clCreateCommandQueue(context, device, 0, &err);
+
+  // 5. Build program from embedded source
+  size_t length = std::strlen(kernelSrc);
+  const char* srcPtr = kernelSrc;
+  cl_program program = clCreateProgramWithSource(context, 1, &srcPtr, &length, &err);
+  err = clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr);
+
+  if (err != CL_SUCCESS) {
+    size_t logSize = 0;
+    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, nullptr, &logSize);
+    std::vector<char> log(logSize);
+    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, logSize, log.data(), nullptr);
+    throw std::runtime_error("clBuildProgram failed:\n" + std::string(log.data()));
+  }
+
+  cl_kernel kernel = clCreateKernel(program, "vec_add", &err);
+
+  // 6. Create buffers
+  cl_mem bufA = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * N,
+                               A.data(), &err);
+  cl_mem bufB = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * N,
+                               B.data(), &err);
+  cl_mem bufC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * N, nullptr, &err);
+
+  // 7. Set arguments and launch kernel
+  clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufA);
+  clSetKernelArg(kernel, 1, sizeof(cl_mem), &bufB);
+  clSetKernelArg(kernel, 2, sizeof(cl_mem), &bufC);
+
+  size_t globalWorkSize = N;
+  err = clEnqueueNDRangeKernel(queue, kernel, 1, nullptr, &globalWorkSize, nullptr, 0, nullptr,
+                               nullptr);
+  clFinish(queue);
+
+  // 8. Read results
+  clEnqueueReadBuffer(queue, bufC, CL_TRUE, 0, sizeof(float) * N, C.data(), 0, nullptr, nullptr);
+
+  // 9. Print results
+  for (int i = 0; i < N; ++i) {
+    std::cout << "C[" << i << "] = " << C[i] << '\n';
+  }
+
+  // 10. Cleanup
+  clReleaseMemObject(bufA);
+  clReleaseMemObject(bufB);
+  clReleaseMemObject(bufC);
+  clReleaseKernel(kernel);
+  clReleaseProgram(program);
+  clReleaseCommandQueue(queue);
+  clReleaseContext(context);
+}
+
 }  // namespace test
 }  // namespace core
