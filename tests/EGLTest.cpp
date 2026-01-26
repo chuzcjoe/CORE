94ca2c966931194a78fe4f49a12dad7075f204a4
chuzcjoe <chuzcjoe@gmail.com>
2026-01-25 21:05:39 -0800

    add egl lib

---
+++ tests/EGLTest.cpp
@@ -0,0 +1,15 @@
+#include <EGL/eglplatform.h>
+#include <glad/glad_egl.h>
+#include <gtest/gtest.h>
+
+#include <string>
+
+namespace core {
+namespace test {
+
+TEST(EGL, test) {
+  // Test implementation goes here
+}
+
+}  // namespace test
+}  // namespace core
