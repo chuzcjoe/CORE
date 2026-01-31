605fec8b53f6519c432595099b664cdae34f4d34
Joe <44826941+chuzcjoe@users.noreply.github.com>
2026-01-30 21:53:36 -0800

    Test GLES on Android devices (#72)
    * Add GLES test
    
    * add triangle test
    
    * update
    
    * add StbImpl.cpp
    
    ---------
    
    Co-authored-by: chuzcjoe <chuzcjoe@gmail.com>

--- tests/StbTest.cpp
+++ tests/StbTest.cpp
@@ -1,12 +1,8 @@
 #include <gtest/gtest.h>
+#include <stb_image.h>
+#include <stb_image_write.h>
 
 #include <string>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
 
 #include "Mat.h"
 
