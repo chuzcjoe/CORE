5fbf432802b49f9b327d3c32a948c94b7c46941a
chuzcjoe <chuzcjoe@gmail.com>
2026-09-11 10:52:54 -0700

    fix compile

--- vulkan/src/VulkanSwapChain.cpp
+++ vulkan/src/VulkanSwapChain.cpp
@@ -86,14 +86,13 @@ void VulkanSwapChain::UnInit() {
 }
 
 VkResult VulkanSwapChain::Present(uint32_t image_index, VkSemaphore wait_semaphore) const {
-  const VkPresentInfoKHR present_info{
-      .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
-      .waitSemaphoreCount = 1,
-      .pWaitSemaphores = &wait_semaphore,
-      .swapchainCount = 1,
-      .pSwapchains = &swapchain,
-      .pImageIndices = &image_index,
-  };
+  VkPresentInfoKHR present_info{};
+  present_info.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+  present_info.waitSemaphoreCount = 1;
+  present_info.pWaitSemaphores = &wait_semaphore;
+  present_info.swapchainCount = 1;
+  present_info.pSwapchains = &swapchain;
+  present_info.pImageIndices = &image_index;
   return vkQueuePresentKHR(context_->present_queue(), &present_info);
 }
 
